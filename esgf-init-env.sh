7ed740b68035464aabd94974f6eec22d26343952
Jason Boutte <boutte.jason@gmail.com>
2019-11-01 19:11:28 -0700

    Updates the init script

---
+++ esgf-init-env.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+LOCK_FILE="${HOME}/.esgf-init-done"
+
+[[ -e "${LOCK_FILE}" ]] && echo "Environment already initialized" && exit 0
+
+echo -e "envs_dirs:\n - /home/jovyan/conda-envs" > /home/jovyan/.condarc
+
+conda init bash
+
+cat << EOF > .esgf-functions.sh
+#!/bin/bash
+
+function esgf-update {
+  mkdir -p ${HOME}/getting_started
+
+  cd ${HOME}/getting_started
+
+  curl https://raw.githubusercontent.com/esgf-nimbus/getting_started/master/getting_started.ipynb -O
+  curl https://raw.githubusercontent.com/esgf-nimbus/getting_started/master/esgf_search.ipynb -O
+}
+EOF
+
+BASHRC="${HOME}/.bashrc"
+SOURCE_LINE=". ${HOME}/.esgf-functions.sh"
+
+[[ ! $(grep "${SOURCE_LINE}" "${BASHRC}") ]] && echo "${SOURCE_LINE}" >> "${BASHRC}"
+
+. "${HOME}/.esgf-functions.sh"
+
+esgf-update
+
+# touch "${LOCK_FILE}"
